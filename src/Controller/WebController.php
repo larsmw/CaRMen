370ea3b6fb28dda56904ed4fa8a63b70a4eacebd
Lars Nielsen <larsnielsen2@gmail.com>
2025-04-26 01:16:34 +0200

    Add registration form and front page controller

---
+++ src/Controller/WebController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class WebController extends AbstractController
+{
+    #[Route('/', name: 'app_web')]
+    public function index(): Response
+    {
+        return $this->render('web/index.html.twig', [
+            'controller_name' => 'WebController',
+        ]);
+    }
+}
--- src/Repository/MenuItemRepository.php
+++ src/Repository/MenuItemRepository.php
@@ -16,28 +16,28 @@ class MenuItemRepository extends ServiceEntityRepository
         parent::__construct($registry, MenuItem::class);
     }
 
-    //    /**
-    //     * @return MenuItem[] Returns an array of MenuItem objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('m.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    /**
+     * @return MenuItem[] Returns an array of MenuItem objects
+     */
+    public function findByMenuField($value): array
+    {
+        return $this->createQueryBuilder('m')
+            ->andWhere('m.menu = :val')
+            ->setParameter('val', $value)
+            ->orderBy('m.id', 'ASC')
+            //->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+            ;
+    }
 
-    //    public function findOneBySomeField($value): ?MenuItem
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function findOneByMenu($value): ?MenuItem
+    {
+      return $this->createQueryBuilder('m')
+          ->andWhere('m.menu = :val')
+          ->setParameter('val', $value)
+          ->getQuery()
+          ->getOneOrNullResult()
+          ;
+    }
 }
