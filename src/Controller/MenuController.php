a4de078217e844c8a730b4bd03f9a78e6f52755b
Lars Nielsen <larsnielsen2@gmail.com>
2025-05-29 16:03:41 +0200

    Fix getting menuitem

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -12,26 +12,22 @@ class MenuController extends AbstractController
 {
 
     public function mainMenu(EntityManagerInterface $em) : Response {
-        $menuitems = $this->getItems($em, 'main');
-        $menu = [];
-        foreach ($menuitems as $item) {
-
-            if ($this->isGranted(MenuItemVoter::VIEW, $item)) {
-              $menu[] = [
-                  'url' => $item->getRoute(),
-                  'name' => $item->getName(),
-                  'title' => $item->getTitle(),
-              ];
-            }
-        }
+        $menu = $this->get($em, 'main');
         return $this->render('menu/main.html.twig',
             ['menu' => $menu]);
     }
 
     public function adminMenu(EntityManagerInterface $em): Response {
-        $menuitems = $this->getItems($em, 'admin');
+        $menu = $this->get($em, 'admin');
+        return $this->render('menu/main.html.twig',
+            ['menu' => $menu]);
+    }
+
+    public function get(EntityManagerInterface $em, string $menuname) : array {
+        $menuitems = $this->getItems($em, $menuname);
         $menu = [];
         foreach ($menuitems as $item) {
+
             if ($this->isGranted(MenuItemVoter::VIEW, $item)) {
               $menu[] = [
                   'url' => $item->getRoute(),
@@ -40,15 +36,11 @@ class MenuController extends AbstractController
               ];
             }
         }
-        return $this->render('menu/main.html.twig',
-            ['menu' => $menu]);
+        return $menu;
     }
 
     private function getItems($em, $menu) : array {
         $entityRepository = $em->getRepository(MenuItem::class);
         return $entityRepository->findByMenuName($menu);
-    }
-    
-    public function get($menuname) : null {
     }
 }
