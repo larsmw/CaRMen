b315ee4a2066c00499fa55b15e35050651f8c04b
Lars Nielsen <larsnielsen2@gmail.com>
2025-06-02 23:28:37 +0200

    Some kind of Rest api start

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
---
+++ src/Controller/RestUserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\CustomerRepository;
+use App\Entity\Customer;
+
+/**
+ * Rest User Controller
+ * @Route("/api", name="api_")
+ */
+class RestUserController extends AbstractFOSRestController {
+
+    /**
+     * List users
+     * @Rest\Get("/users")
+     *
+     * @return Response
+     */
+    #[Route('/api/users', name: 'api_users')]
+    public function getUsersAction(Request $request, CustomerRepository $customerRepository) {
+        error_log(__METHOD__);
+        $customers = $customerRepository->findAll();
+        return $this->handleView($this->view($customers));
+    }
+}
