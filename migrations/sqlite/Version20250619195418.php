e4907cd190f28a1470d880b365334bd8c8a45e27
Lars Nielsen <larsnielsen2@gmail.com>
2025-06-19 23:06:22 +0200

    try to remove collate

--- migrations/sqlite/Version20250619195418.php
+++ migrations/sqlite/Version20250619195418.php
@@ -21,28 +21,28 @@ final class Version20250619195418 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            CREATE TABLE address (id INT AUTO_INCREMENT NOT NULL, address_line1 VARCHAR(255) NOT NULL, address_line2 VARCHAR(255) DEFAULT NULL, postalcode VARCHAR(12) NOT NULL, PRIMARY KEY(id)) COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+            CREATE TABLE address (id INT AUTO_INCREMENT NOT NULL, address_line1 VARCHAR(255) NOT NULL, address_line2 VARCHAR(255) DEFAULT NULL, postalcode VARCHAR(12) NOT NULL, PRIMARY KEY(id))
         SQL);
         $this->addSql(<<<'SQL'
-            CREATE TABLE customer (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, phone VARCHAR(16) NOT NULL, PRIMARY KEY(id)) COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+            CREATE TABLE customer (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, phone VARCHAR(16) NOT NULL, PRIMARY KEY(id)) 
         SQL);
         $this->addSql(<<<'SQL'
-            CREATE TABLE menu (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, machine_name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+            CREATE TABLE menu (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, machine_name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) 
         SQL);
         $this->addSql(<<<'SQL'
-            CREATE TABLE menu_item (id INT AUTO_INCREMENT NOT NULL, parent INT NOT NULL, name VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, route VARCHAR(255) NOT NULL, menu VARCHAR(255) NOT NULL, PRIMARY KEY(id)) COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+            CREATE TABLE menu_item (id INT AUTO_INCREMENT NOT NULL, parent INT NOT NULL, name VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, route VARCHAR(255) NOT NULL, menu VARCHAR(255) NOT NULL, PRIMARY KEY(id)) 
         SQL);
         $this->addSql(<<<'SQL'
-            CREATE TABLE permission (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, action VARCHAR(255) NOT NULL, subject VARCHAR(255) NOT NULL, PRIMARY KEY(id)) COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+            CREATE TABLE permission (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, action VARCHAR(255) NOT NULL, subject VARCHAR(255) NOT NULL, PRIMARY KEY(id)) 
         SQL);
         $this->addSql(<<<'SQL'
-            CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, permissions_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_57698A6A9C3E4F87 (permissions_id), PRIMARY KEY(id)) COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+            CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, permissions_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_57698A6A9C3E4F87 (permissions_id), PRIMARY KEY(id))
         SQL);
         $this->addSql(<<<'SQL'
-            CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT '(DC2Type:json)', password VARCHAR(255) NOT NULL, is_verified TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), UNIQUE INDEX UNIQ_IDENTIFIER_USERNAME (username), PRIMARY KEY(id)) COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+            CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT '(DC2Type:json)', password VARCHAR(255) NOT NULL, is_verified TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), UNIQUE INDEX UNIQ_IDENTIFIER_USERNAME (username), PRIMARY KEY(id))
         SQL);
         $this->addSql(<<<'SQL'
-            CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', available_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', delivered_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+            CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', available_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', delivered_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) 
         SQL);
         $this->addSql(<<<'SQL'
             ALTER TABLE role ADD CONSTRAINT FK_57698A6A9C3E4F87 FOREIGN KEY (permissions_id) REFERENCES permission (id)
