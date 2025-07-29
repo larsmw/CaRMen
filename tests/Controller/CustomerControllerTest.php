4e554485e6278b0df0f45cf42037cc4f63f9c0f4
Lars Nielsen <larsnielsen2@gmail.com>
2025-07-29 21:57:26 +0200

    fix test url expect 301

--- tests/Controller/CustomerControllerTest.php
+++ tests/Controller/CustomerControllerTest.php
@@ -12,6 +12,6 @@ final class CustomerControllerTest extends WebTestCase
         $client = static::createClient();
         $client->request('GET', 'http://localhost/customer');
 
-        self::assertResponseIsSuccessful();
+        self::assertResponseRedirects('http://localhost/customer', 301);
     }
 }
