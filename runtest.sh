84c1361382ad13cf20deea7385421a5405e2fad2
Lars Nielsen <lars@lfweb.dk>
2026-03-28 21:56:25 +0100

    Run phpunit on push

--- runtest.sh
+++ runtest.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-docker compose exec backend sh -c 'APP_ENV=test php bin/phpunit --testdox'
+
+docker compose exec backend sh -c '
+    APP_ENV=test php bin/console doctrine:database:create &&
+    APP_ENV=test php bin/console doctrine:migrations:migrate --no-interaction &&
+    APP_ENV=test php bin/console doctrine:fixtures:load --no-interaction &&
+    APP_ENV=test php bin/phpunit --testdox
+  '
 
